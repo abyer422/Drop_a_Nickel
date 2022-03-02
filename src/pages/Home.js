ed2cbc85fe644d202908dac51f757c8a600ab914
abyer422 <abyer422@gmail.com>
2022-03-01 20:39:20 -0700

    added new home page image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import BannerImage from '../assets/bowlingHome2.jpg';
+// import BannerImage from '../assets/bowlingHome2.jpg';
+import BannerImage from '../assets/bowlingHome.jpg';
 import '../styles/Home.css';
 
 function Home() {
